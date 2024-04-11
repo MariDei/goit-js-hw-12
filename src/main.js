e3202ea5a010d447329138ed590b165458bdc6f9
Marianna Dei <d.marianna@icloud.com>
2024-04-12 01:15:24 +0300

    Added the iziToast, SimpleLightbox, css-loader and Axios libraries.

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// Підключення бібліотеки iziToast
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+// Підключення бібліотеки SimpleLightbox
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Підключення бібліотеки Axios
+import axios from 'axios';
